d6ba492dd5a5878293c6b0b3737f883337fb4759
ismahd95 <ihdominguez905@gmail.com>
2019-09-07 22:47:34 -0400

    Add Adminsb material design template and fix some stuff
    Fix preventDefault inside passive event listener from jquery.slimscroll
    Add navbar component
    Add left-sidebar component
    Add right-sidebar component
    Add search-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
+import { NavbarComponent } from './main-layout/navbar/navbar.component';
+import { LeftSidebarComponent } from './main-layout/left-sidebar/left-sidebar.component';
+import { RightSidebarComponent } from './main-layout/right-sidebar/right-sidebar.component';
+import { SearchBarComponent } from './main-layout/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    NavbarComponent,
+    LeftSidebarComponent,
+    RightSidebarComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule
--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  pageTitle: string = 'Stuff management';
+  products: Array<any> = [];
+
   constructor() { }
 
   ngOnInit() {
